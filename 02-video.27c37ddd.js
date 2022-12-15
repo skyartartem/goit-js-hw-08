948d39ac1e9bfa58a5e29d77cc803084f24643c1
skyartartem <109517397+skyartartem@users.noreply.github.com>
2022-12-15 23:53:32 +0000

    Deploying to gh-pages from @ skyartartem/goit-js-hw-08@00a9abce23460c0c3e1581b87e9d67a069eadaa3 🚀

--- 02-video.27c37ddd.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="red",console.log("start");
-//# sourceMappingURL=02-video.27c37ddd.js.map
--- 02-video.2a630796.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="red",console.log("start");
-//# sourceMappingURL=02-video.2a630796.js.map
