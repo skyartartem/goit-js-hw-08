fadc202d062e6d0fd1b8921022f15135390313e0
Artem Riabchych <artemskyart@gmail.com>
2022-12-16 00:09:24 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector("#vimeo-player");
+
+// const player = new Player(iframe);
+
+document.body.style.backgroundColor = "red";
+
+console.log("start");
+
+// console.dir(player);
+
+// const player = new Player('handstick', {
+//     // id: 19231868,
+//     // width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+// const a;
